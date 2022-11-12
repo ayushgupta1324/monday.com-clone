aec770f818f6649d1e2cc0ae9b210d31e290e998
ayushgupta1324 <ayushgupta1324@gmail.com>
2022-11-12 22:42:22 +0530

    day-5 admin page

--- scripts/admin.js
+++ scripts/admin.js
@@ -6,7 +6,8 @@ details_button.onclick =async () => {
     data = await res.json();
 
     append(data)
-    performOperations(data);
+    person_filter(data);
+    status_filter(data);
 }
 
 function append(data)
@@ -14,10 +15,10 @@ function append(data)
 
     let details_div = document.querySelector(".details-wrapper");
     details_div.innerHTML=null;
-
-    
-    
     data.forEach(element => {
+
+        if(element.id!=1)
+        {
         let employee_div = document.createElement("div");
         employee_div.setAttribute("class", "employee_div")
 
@@ -33,18 +34,21 @@ function append(data)
         status_name.style.color = "white";
         if(status_name.innerText == "working on it")
         {
-            status_name.style.backgroundColor = "coral"
-            employee_div.style.border="1px solid coral";
+            status_name.style.backgroundColor = "#FDAB3D"
+            employee_div.style.border="3px solid #FDAB3D";
+            employee_div.style.borderRadius="8px"
         }
         else if(status_name.innerText == "Done")
         {
-            status_name.style.backgroundColor = "green"
-            employee_div.style.border="1px solid green";
+            status_name.style.backgroundColor = "#00C875"
+            employee_div.style.border="3px solid #00C875";
+            employee_div.style.borderRadius="8px"
         }
         else
         {
-            status_name.style.backgroundColor = "crimson";
-            employee_div.style.border="1px solid crimson";
+            status_name.style.backgroundColor = "#E2445C";
+            employee_div.style.border="3px solid #E2445C";
+            employee_div.style.borderRadius="8px"
         }
 
         let date_added = document.createElement("p");
@@ -56,17 +60,67 @@ function append(data)
        
         employee_div.append(employee_name,task_name,status_name,department,date_added)        
         details_div.append(employee_div);
+    }
     });
+    
 }
 
 
-function performOperations(data)
+function person_filter(data)
 {
-var myParent = document.body;
-let array = []
+var person_filter_append = document.getElementById("person-filter-append");
+person_filter_append.innerHTML=null;
+let array = [];
+let dropdown_name = new Set();
+
 data.forEach(el=>{
     let name = el.name;
-    array.push(name)
+    
+    if(!dropdown_name.has(el.name))
+    {
+        dropdown_name.add(el.name)
+        array.push(name)
+    }
+})
+
+//Create and append select list
+var selectList = document.createElement("select");
+selectList.id = "mySelect";
+
+//Create and append the options
+for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    if(array[i]!=undefined)
+    {
+        option.value = array[i];
+        option.text = array[i];
+        selectList.appendChild(option);
+    } 
+}
+person_filter_append.appendChild(selectList);
+
+selectList.addEventListener("change", function(){
+    showResult(selectList.value)
+})
+
+}
+
+function status_filter(data)
+{
+    var status_filter_append = document.getElementById("status-filter-append")
+    status_filter_append.innerHTML=null;
+
+    // var myParent = document.body;
+let array = [];
+const dropdown_status = new Set();
+data.forEach(el=>{
+    let status = el.status;
+    
+    if(!dropdown_status.has(el.status))
+    {
+        dropdown_status.add(el.status)
+        array.push(status);
+    }
 })
 
 
@@ -75,21 +129,29 @@ data.forEach(el=>{
 //Create and append select list
 var selectList = document.createElement("select");
 selectList.id = "mySelect";
-myParent.appendChild(selectList);
+
+
 
 //Create and append the options
 for (var i = 0; i < array.length; i++) {
     var option = document.createElement("option");
-    option.value = array[i];
-    option.text = array[i];
-    selectList.appendChild(option);
+
+    if(array[i]!=undefined)
+    {
+        option.value = array[i];
+        option.text = array[i];
+        selectList.appendChild(option);
+    }
+    
 }
+status_filter_append.appendChild(selectList);
+
+
 selectList.addEventListener("change", function(){
-    showResult(selectList.value)
+    status_filter_result(selectList.value)
 })
 
 }
-
 
 function showResult(data_passed)
 {
@@ -105,3 +167,16 @@ function showResult(data_passed)
         append(array_data)
 }
 
+function status_filter_result(data_passed)
+{
+    let array_data=[];
+    console.log(data_passed)
+    data.forEach(el=>{
+        if(el.status==data_passed)
+        {
+            array_data.push(el)
+            console.log(el); 
+        }})
+
+        append(array_data)
+}
